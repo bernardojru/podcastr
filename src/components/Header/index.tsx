0d88fbc4c5d78e431c7708a9ee1eb0f9ed240c1b
bernardojru <bernardogomes860@gmail.com>
2023-02-27 15:26:05 +0100

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -16,38 +16,51 @@ import { prisma } from "../../lib/prisma";
 import { ToggleThemesButton } from "../ToggleThemesButton";
 import { ArrowLeft } from "phosphor-react";
 import axios from "axios";
-import { string } from "zod/lib";
+import { string, z } from "zod/lib";
 import { Avatar } from "../Avatar";
 import { api, server } from "../../lib/axios";
 
 interface User {
-  id: string
+  id: string;
   name: string;
   password: string;
   email: string;
 }
 
+const nameChema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "O nome precisa ter pelo menos 4 letras" }),
+});
+
+type nameType = z.infer<typeof nameChema>;
+
 export function Header() {
   const { themes } = useThemes();
   const [user, setUser] = useState<User | null>(null);
+  const [] = useState()
 
-  async function handleGetUserInfo () {
-    const {data} = await server.get('/api/')
+  async function handleGetUserInfo() {
+    const { data } = await server.get("/api/", {
+      params: {
+        name
+      },
+    });
 
-    setUser(data)
-    console.log(data, 'aquiiiiiiiiiiiiiii!')
+    setUser(data);
+    console.log(data, "aquiiiiiiiiiiiiiii!");
   }
 
   useEffect(() => {
-    handleGetUserInfo()
-  }, []) 
+    handleGetUserInfo();
+  }, []);
 
   const [isOpen, setIsOpen] = useState(false);
 
   function openExitButton() {
     setIsOpen(!isOpen);
   }
-  
+
   return (
     <HeaderContainer className={themes}>
       <div>
